ce592a485cef973f4d3ee62698277211ccd9539f
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2025-01-20 23:30:21 +0530

    add hints to sidebar items

--- canvasly/app/(dashboard)/_components/sidebar/item.tsx
+++ canvasly/app/(dashboard)/_components/sidebar/item.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
+import { Hint } from "@/components/hint";
 
 interface ItemProps {
   id: string;
@@ -26,16 +27,23 @@ export function Item({ id, imageUrl, name }: ItemProps) {
 
   return (
     <div className="aspect-square relative">
-      <Image
-        src={imageUrl}
-        onClick={onClick}
-        alt={name}
-        fill
-        className={cn(
-          "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
-          isActive && "opacity-100"
-        )}
-      />
+      <Hint
+        label={name}
+        side="right"
+        align="start"
+        sideOffset={18}
+      >
+        <Image
+          src={imageUrl}
+          onClick={onClick}
+          alt={name}
+          fill
+          className={cn(
+            "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+            isActive && "opacity-100"
+          )}
+        />
+      </Hint>
     </div>
   );
 }
